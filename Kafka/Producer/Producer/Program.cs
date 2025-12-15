9582754b05b7c4552f7b55292373b510b4c6e54a
Samer <staalab@gmsil.com>
2025-12-15 11:23:27 -0500

    add prog

--- Kafka/Producer/Producer/Program.cs
+++ Kafka/Producer/Producer/Program.cs
@@ -1,3 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Confluent.Kafka;
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
 
-Console.WriteLine("Hello, World!");
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string bootstrapServers = "localhost:9092"; // Your local Kafka broker address
+            string topicName = "my-topic";
+
+            // Producer example
+            var producerConfig = new ProducerConfig { BootstrapServers = bootstrapServers, ClientId = topicName };
+            using (var producer = new ProducerBuilder<Null, string>(producerConfig).Build())
+            {
+                var message = new Message<Null, string> { Value = "Hello Kafka from .NET!" };
+                var n = 0;
+                while (n<10)
+                {
+                    n += 1;
+                    var deliveryReport = await producer.ProduceAsync(topicName, message);
+                    Console.WriteLine($"Delivered '{deliveryReport.Value}' to '{deliveryReport.TopicPartitionOffset}'");
+                }
+            }
+
+            // Consumer example
+            var consumerConfig = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServers,
+                GroupId = "my-consumer-group",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build())
+            {
+                consumer.Subscribe(topicName);
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true; // Prevent the app from exiting immediately
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var consumeResult = consumer.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{consumeResult.Message.Value}' at: '{consumeResult.TopicPartitionOffset}'");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error consuming: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Consumer was cancelled
+                }
+                finally
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
